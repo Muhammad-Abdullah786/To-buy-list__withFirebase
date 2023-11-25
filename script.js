1eaee2c090771fc836fe6ea0f330d97d47418540
Muhamad Abdullah <abdulx125@gmail.com>
2023-11-25 13:53:21 +0500

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('btn')
+let userInput = document.getElementById('usrInput')
+
+button.addEventListener('click', function(){
+    console.log('clicked', userInput.value);
+})
+
