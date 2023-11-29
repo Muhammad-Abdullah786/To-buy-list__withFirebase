c7f199ed25fb637c0528ccc3531f3fcf4a9e7a21
Muhamad Abdullah <abdulx125@gmail.com>
2023-11-30 00:36:53 +0500

    database error

---
+++ functions.js
@@ -0,0 +1,5 @@
+// this contain funtion which i can export
+
+export function add (a, b){
+    return a+b
+}
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";// the add is the funtion i made in funtion.js
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-dataBase.js";// the add is the funtion i made in funtion.js
+
+const appSetting ={
+    dataBaseUrl:"https://add-product-b6e2a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+let app = initializeApp(appSetting)
+let dataBase = getDatabase(app)
+console.log(app);
+
+
+
+
 let button = document.getElementById('btn')
 let userInput = document.getElementById('usrInput')
 
